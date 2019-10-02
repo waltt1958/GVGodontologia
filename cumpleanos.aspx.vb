71398cfbd2649d04bae2560a6e0243b27fd27b0c
waltt1958 <ovidiov1958@gmail.com>
2019-10-02 17:45:04 -0300

    avanza el correo

--- cumpleanos.aspx.vb
+++ cumpleanos.aspx.vb
@@ -17,14 +17,53 @@ Partial Class cumpleanosDefault
 
     Protected Sub GridView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles GridView1.SelectedIndexChanged
 
-        Dim mensaje, saludo, saludoFinal, direccion As String
+        Dim saludo, despedida, firma, especialidad, direccion As String
 
-        mensaje = "GVG Odongologia le desea que pase un muy feliz cumpleaños."
-        saludo = "Afectuoso saludo de"
-        saludoFinal = " Dra. Guadalupe Vicente Galan"
-        direccion = "España xxx - Rosario // Telefono: 4251589"
+        Dim mensaje As New System.Net.Mail.MailMessage()
+        Dim smtp As New System.Net.Mail.SmtpClient()
+
+        'configuracion smtp
+
+        smtp.Credentials = New System.Net.NetworkCredential("ovidiov1958@gmail.com", "corazones1958")
+        smtp.Host = "smtp.gmail.com"
+        smtp.Port = 587 ' 465
+        smtp.EnableSsl = True
 
 
+        'configuracion del mensaje
+
+        saludo = "GVG Odongologia le desea que pase un muy feliz cumpleaños."
+        ' despedida = "Afectuoso saludo de"
+        'firma = " Dra. Guadalupe Vicente Galan"
+        'especialidad = " Odontóloga Especialista en Estética Dental"
+        'direccion = "España 505 - 1er Piso - Rosario // Telefono: 4251589"
+
+        'correo al que le envío el mail
+        Dim mailPara As String
+        mailPara = GridView1.Rows(GridView1.SelectedIndex).Cells(7).Text
+        mensaje.To.Add(mailPara.ToString)
+
+        'quien envía el correo
+        mensaje.From = New System.Net.Mail.MailAddress("ovidiov1958@gmail.com", "Dra. Guadalupe Vicente Galan", System.Text.Encoding.UTF8)
+
+        'asunto del mail
+        mensaje.Subject = "SALUTACION POR SU CUMPLEAÑOS"
+
+        'codificacion
+        mensaje.SubjectEncoding = System.Text.Encoding.UTF8
+
+        'contenido del mail
+        mensaje.Body = saludo.ToString
+        'mensaje.Body = despedida.ToString
+        'mensaje.Body = firma.ToString
+        'mensaje.Body = especialidad.ToString
+        'mensaje.Body = direccion.ToString
+
+        mensaje.BodyEncoding = System.Text.Encoding.UTF8
+        mensaje.Priority = System.Net.Mail.MailPriority.Normal
+        mensaje.IsBodyHtml = True
+
+        smtp.Send(mensaje)
 
 
     End Sub
