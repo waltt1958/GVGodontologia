370d2a40bb8478d252bf37e7851f8863ba5b1b33
waltt1958 <ovidiov1958@gmail.com>
2019-10-05 18:11:11 -0300

    final correo electronico

--- cumpleanos.aspx.vb
+++ cumpleanos.aspx.vb
@@ -48,27 +48,50 @@ Partial Class cumpleanosDefault
 
         'quien envía el correo
         mensaje.From = New System.Net.Mail.MailAddress("ovidiov1958@gmail.com", "Dra. Guadalupe Vicente Galan", System.Text.Encoding.UTF8)
-
+        Dim nombre As String
         'a donde se responden los mails
         mensaje.ReplyToList.Add("ovidiov1958@gmail.com")
+        nombre = GridView1.Rows(GridView1.SelectedIndex).Cells(0).Text.ToString
 
         'asunto del mail
-        mensaje.Subject = "SALUTACION POR SU CUMPLEAÑOS"
+
+        Dim asunto As String
+        asunto = "SALUTACION POR SU CUMPLEAÑOS Sr/a  " & nombre
+        mensaje.Subject = asunto ' "SALUTACION POR SU CUMPLEAÑOS"
 
         'codificacion
         mensaje.SubjectEncoding = System.Text.Encoding.UTF8
 
-        'contenido del mail
-        mensaje.Body = saludo.ToString
-        mensaje.Body = texto.ToString
-        mensaje.Body = despedida.ToString
-        mensaje.Body = firma.ToString
-        mensaje.Body = especialidad.ToString
-        mensaje.Body = direccion.ToString
+        'TODO ESTO ES PARA DARLE FORMA AL MENSAJE 
+        'aca forma para texto plano
 
-        mensaje.BodyEncoding = System.Text.Encoding.UTF8
-        mensaje.Priority = System.Net.Mail.MailPriority.Normal
-        mensaje.IsBodyHtml = True
+
+        Dim plano As String = "GVG Odongologia le desea que pase un muy feliz cumpleaños en compañía de sus afectos" & vbCrLf & vbCrLf _
+         & "Dado que la salud bucal es nuestra prioridad, lo invitamos a comunicarse al teléfono 4251589" & vbCrLf _
+         & "para obtener un turno a fin de que le realicemos un control de su salud bucal sin cargo. " & vbCrLf & vbCrLf _
+         & "Afectuoso saludo de" & vbCrLf _
+         & "Dra. Guadalupe Vicente Galan" & vbCrLf _
+         & "Odontóloga Especialista en Estética Dental" & vbCrLf _
+         & "Universidad de Rosario y Universidad de Buenos Aires" & vbCrLf _
+         & "España 505 - 1er Piso - Rosario - Telefono 4251589"
+
+        Dim plainView As AlternateView = AlternateView.CreateAlternateViewFromString(plano, Nothing, System.Net.Mime.MediaTypeNames.Text.Plain)
+
+        'texto html
+        Dim html As String = "<h2><B>GVGodontología le desea que pase un muy feliz cumpleaños en compañia de sus afectos</b></h2>" & vbCrLf _
+         & "Dado que la salud bucal es nuestra prioridad, lo invitamos a comunicarse al teléfono 4251589<br>" _
+         & "para obtener un turno a fin de que le realicemos un control de su salud bucal sin cargo.<br><br> " _
+         & "Afectuoso saludo de<br>" _
+         & "<b>Dra. Guadalupe Vicente Galan<br>" _
+         & "<b>Odontóloga Especialista en Estética Dental - Mat. Nacional xxxxxx<br>" _
+         & "<b>Universidad de Rosario y Universidad de Buenos Aires<br>" _
+         & "<b>España 505 - 1er Piso - Rosario - Telefono 4251589img"
+
+        Dim htmlview = AlternateView.CreateAlternateViewFromString(html, Nothing, System.Net.Mime.MediaTypeNames.Text.Html)
+
+        mensaje.AlternateViews.Add(plainView)
+        mensaje.AlternateViews.Add(htmlview)
+        'fin de darle forma al mensaje
 
         Try
 
@@ -97,8 +120,6 @@ Partial Class cumpleanosDefault
             ClientScript.RegisterClientScriptBlock(Me.GetType(), "alert", sb.ToString())
 
         End Try
-
-
 
     End Sub
 
